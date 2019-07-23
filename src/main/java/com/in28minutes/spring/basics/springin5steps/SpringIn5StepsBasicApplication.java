f130c4ef229f1a4fa5bda591a3fd87a2f1173555
System Administrator <root@Maxs-MacBook-Pro.local>
2019-07-23 14:22:41 -0400

    add logback and close application context

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -12,17 +12,20 @@ public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext applicationContext =
-				new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);
+		try(AnnotationConfigApplicationContext applicationContext =
+					new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class)){
 
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		//BinarySearchImpl  binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+			//BinarySearchImpl  binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
 
-		System.out.println(binarySearch);
-		//System.out.println(binarySearch1);
+			System.out.println(binarySearch);
+			//System.out.println(binarySearch1);
 
-		int result = binarySearch.binarySearch(new int[]{12,4,6},3);
-		System.out.println(result);
+			int result = binarySearch.binarySearch(new int[]{12,4,6},3);
+			System.out.println(result);
+		}
+
+
 	}
 
 }
